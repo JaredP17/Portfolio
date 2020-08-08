012a44d1bf9208849de19cd4adc6f5f1adc9be0e
Jared Porter <jaredp17@me.com>
2020-08-07 21:45:14 -0400

    Setup basic app, components, pages, routes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import { Container } from "reactstrap";
+import AboutMe from "./pages/AboutMe/AboutMe";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Container>
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Container>
+      </div>
+    </Router>
   );
 }
 
