f146339ec05528a48a8aedece0e406148bbd6dbf
Jared Porter <jaredp17@me.com>
2020-08-10 11:47:00 -0400

    Renamed NavTabs.js to NavBar.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import About from "./components/pages/About";
-import Projects from "./components/pages/Projects"
+import Projects from "./components/pages/Projects";
 import Contact from "./components/pages/Contact";
 
 function App() {
