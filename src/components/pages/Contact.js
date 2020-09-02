2772384343f4565220ad91c488e1a90da1a39a55
Jared Porter <jaredp17@me.com>
2020-09-02 13:09:27 -0400

    Fixed instances of class to className

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -15,25 +15,25 @@ function Contact(props) {
                   href="mailto:jaredporter.dev@icloud.com"
                   className="list-group-item list-group-item-action"
                 >
-                  E-mail<i class="fa fa-envelope" aria-hidden="true"></i>
+                  E-mail<i className="fa fa-envelope" aria-hidden="true"></i>
                 </a>
                 <a href="https://github.com/jaredp17"
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="list-group-item list-group-item-action">
-                  GitHub<i class="fa fa-github" aria-hidden="true"></i>
+                  GitHub<i className="fa fa-github" aria-hidden="true"></i>
                 </a>
                 <a href="https://www.linkedin.com/in/jared-r-porter/" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="list-group-item list-group-item-action">
-                  LinkedIn<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                  LinkedIn<i className="fa fa-linkedin-square" aria-hidden="true"></i>
                 </a>
                 <a href={resume} 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="list-group-item list-group-item-action">
-                  Resume<i class="fa fa-file-text" aria-hidden="true"></i>
+                  Resume<i className="fa fa-file-text" aria-hidden="true"></i>
                 </a>
               </div>
             </article>
