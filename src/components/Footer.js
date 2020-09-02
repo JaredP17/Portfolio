a55084d25f27b1f9d64cc1c2467a6fdebaf90973
Jared Porter <jaredp17@me.com>
2020-09-02 13:08:03 -0400

    Fixed instances of class to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="footer mt-auto py-3 fixed-bottom">
-      <div class="container">
+    <footer className="footer mt-auto py-3 fixed-bottom">
+      <div className="container">
         <span>&copy; 2020 Jared R. Porter</span>
       </div>
     </footer>
